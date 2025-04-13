08fe040db893632db43f983fdc616a8f9e325623
unknown <dwlee0802@gmail.com>
2025-04-13 00:05:46 -0700

    Made Repository and Entity for universities table

---
+++ unicate_server/src/main/java/csci201team25/unicate_server/DateRange.java
@@ -0,0 +1,26 @@
+package csci201team25.unicate_server;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class DateRange {
+	LocalDate startDate;
+	LocalDate endDate;
+	
+	public DateRange (LocalDate start, LocalDate end){
+		startDate = start;
+		endDate = end;
+	}
+	
+	public long getDurationDays() {
+		return ChronoUnit.DAYS.between(startDate, endDate) + 1;
+	}
+	
+	@Override
+	public String toString() {
+		if (getDurationDays() == 1) {
+			return startDate.toString();
+		}
+		return startDate.toString() + " - " + endDate.toString();
+	}
+}
