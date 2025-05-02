fbafd7c2384d0115a060056777b3dbcbf26f6e3e
Andrew Seto <110152659+andrewrsetosj@users.noreply.github.com>
2025-05-02 01:54:26 -0700

    add uni servlet

---
+++ AddUniversitiesServlet.java
@@ -0,0 +1,87 @@
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import javax.json.*;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+@WebServlet("/AddUniversitiesServlet")
+public class AddUniversitiesServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+	public void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		// System.out.println("Servlet CP");
+	    
+		Connection conn = null;
+		Statement st = null;
+		ResultSet rs = null;
+		
+		// Variable information
+		String db = "unicate";
+		String username = "root";
+		String password = "root";		
+		String connection = "jdbc:mysql://localhost/" + db.trim() + "?user=" + username.trim() + "&password=" + password.trim();
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn = DriverManager.getConnection(connection);
+			st = conn.createStatement();
+			rs = st.executeQuery("SELECT uniID, UniversityName FROM Universities");
+			
+			// Sending uni ID and name to frontend as JSON object
+			JsonObjectBuilder json_builder = Json.createObjectBuilder();
+			String id = "";
+			String uni_name = "";
+			while (rs.next()) {
+				id = rs.getString("uniID");
+				uni_name = rs.getString("UniversityName");
+				json_builder.add(id, uni_name);
+			} 
+			// System.out.println("JSON Builder CP: " + json_builder.build().toString());
+			
+			response.setContentType("application/json");
+			response.setCharacterEncoding("UTF-8");
+			response.getWriter().write(json_builder.build().toString());
+		} 
+		
+		// Catching errors 
+		catch (SQLException sqle) {
+			System.out.println("SQLException: " + sqle.getMessage());
+		} 
+		
+		catch (ClassNotFoundException e) {
+			System.out.println ("ClassNotFoundException: " + e.getMessage());
+		} 
+		
+		// Closing objects
+		// https://stackoverflow.com/questions/22671697/try-try-with-resources-and-connection-statement-and-resultset-closing
+		finally {
+		    try {
+		        if (conn != null) conn.close();
+		    } catch (SQLException sqle) {
+		    	System.out.println ("SQLException: " + sqle.getMessage());		    
+		    
+		    }
+		    
+		    try {
+		        if (st != null) st.close();
+		    } catch (SQLException sqle) {
+		    	System.out.println ("SQLException: " + sqle.getMessage());	
+		    }
+		    
+		    try {
+		        if (rs != null) rs.close();
+		    } catch (SQLException sqle) {
+		    	System.out.println ("SQLException: " + sqle.getMessage());	
+		    }
+		}	
+	} 
+}
+
